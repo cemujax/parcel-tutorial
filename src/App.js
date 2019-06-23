457963baff503e3b071251afd39b985efcc66125
cemujax <cemujax@gmail.com>
2019-06-23 21:38:28 +0900

    TypographyJS and Building

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
 import React from "react";
 import styles from "./styles.scss";
-export default () => <div className={styles.title}>App</div>;
+
+export default () => (
+  <div className={styles.title}>
+    <h1>App!</h1>
+    <p>content</p>
+  </div>
+);
